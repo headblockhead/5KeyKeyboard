303d867e1f4d509d09d86fd566cfdefcfedd547f
Headblockhead <headblockhead@gmail.com>
2022-07-24 20:28:26 +0100

    Added all keys

--- code.py
+++ code.py
@@ -14,6 +14,7 @@ import time
 import digitalio
 from board import *
 import pwmio
+import board
 
 defaultDelay = 0
 
@@ -26,27 +27,7 @@ supervisor.disable_autoreload()
 time.sleep(.5)
 
 duckyCommands = {
-    'WINDOWS': Keycode.WINDOWS, 'GUI': Keycode.GUI,
-    'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,
-    'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,
-    'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,
-    'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,
-    'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,
-    'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,
-    'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,
-    'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,
-    'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,
-    'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,
-    'BACKSPACE': Keycode.BACKSPACE,
-    'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,
-    'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,
-    'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,
-    'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,
-    'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,
-    'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,
-    'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,
-    'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,
-    'F12': Keycode.F12,
+    'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E, 'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J, 'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O, 'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T, 'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y, 'Z': Keycode.Z, '1': Keycode.ONE, '2': Keycode.TWO, '3': Keycode.THREE, '4': Keycode.FOUR, '5': Keycode.FIVE, '6': Keycode.SIX, '7': Keycode.SEVEN, '8': Keycode.EIGHT, '9': Keycode.NINE, '0': Keycode.ZERO, 'ENTER': Keycode.ENTER, 'RETURN': Keycode.RETURN, 'ESCAPE': Keycode.ESCAPE, 'BACKSPACE': Keycode.BACKSPACE, 'TAB': Keycode.TAB, 'SPACEBAR': Keycode.SPACEBAR, 'SPACE': Keycode.SPACE, 'MINUS': Keycode.MINUS, 'EQUALS': Keycode.EQUALS, 'LEFT_BRACKET': Keycode.LEFT_BRACKET, 'RIGHT_BRACKET': Keycode.RIGHT_BRACKET, 'BACKSLASH': Keycode.BACKSLASH, 'POUND': Keycode.POUND, 'SEMICOLON': Keycode.SEMICOLON, 'QUOTE': Keycode.QUOTE, 'GRAVE_ACCENT': Keycode.GRAVE_ACCENT, 'COMMA': Keycode.COMMA, 'PERIOD': Keycode.PERIOD, 'FORWARD_SLASH': Keycode.FORWARD_SLASH, 'CAPS_LOCK': Keycode.CAPS_LOCK, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3, 'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7, 'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11, 'F12': Keycode.F12, 'PRINT_SCREEN': Keycode.PRINT_SCREEN, 'SCROLL_LOCK': Keycode.SCROLL_LOCK, 'PAUSE': Keycode.PAUSE, 'INSERT': Keycode.INSERT, 'HOME': Keycode.HOME, 'PAGE_UP': Keycode.PAGE_UP, 'DELETE': Keycode.DELETE, 'END': Keycode.END, 'PAGE_DOWN': Keycode.PAGE_DOWN, 'RIGHT_ARROW': Keycode.RIGHT_ARROW, 'LEFT_ARROW': Keycode.LEFT_ARROW, 'DOWN_ARROW': Keycode.DOWN_ARROW, 'UP_ARROW': Keycode.UP_ARROW, 'KEYPAD_NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'KEYPAD_FORWARD_SLASH': Keycode.KEYPAD_FORWARD_SLASH, 'KEYPAD_ASTERISK': Keycode.KEYPAD_ASTERISK, 'KEYPAD_MINUS': Keycode.KEYPAD_MINUS, 'KEYPAD_PLUS': Keycode.KEYPAD_PLUS, 'KEYPAD_ENTER': Keycode.KEYPAD_ENTER, 'KEYPAD_ONE': Keycode.KEYPAD_ONE, 'KEYPAD_TWO': Keycode.KEYPAD_TWO, 'KEYPAD_THREE': Keycode.KEYPAD_THREE, 'KEYPAD_FOUR': Keycode.KEYPAD_FOUR, 'KEYPAD_FIVE': Keycode.KEYPAD_FIVE, 'KEYPAD_SIX': Keycode.KEYPAD_SIX, 'KEYPAD_SEVEN': Keycode.KEYPAD_SEVEN, 'KEYPAD_EIGHT': Keycode.KEYPAD_EIGHT, 'KEYPAD_NINE': Keycode.KEYPAD_NINE, 'KEYPAD_ZERO': Keycode.KEYPAD_ZERO, 'KEYPAD_PERIOD': Keycode.KEYPAD_PERIOD, 'KEYPAD_BACKSLASH': Keycode.KEYPAD_BACKSLASH, 'APPLICATION': Keycode.APPLICATION, 'POWER': Keycode.POWER, 'KEYPAD_EQUALS': Keycode.KEYPAD_EQUALS, 'F13': Keycode.F13, 'F14': Keycode.F14, 'F15': Keycode.F15, 'F16': Keycode.F16, 'F17': Keycode.F17, 'F18': Keycode.F18, 'F19': Keycode.F19, 'F20': Keycode.F20, 'F21': Keycode.F21, 'F22': Keycode.F22, 'F23': Keycode.F23, 'F24': Keycode.F24, 'LEFT_SHIFT': Keycode.LEFT_SHIFT, 'LEFT_ALT': Keycode.LEFT_ALT, 'RIGHT_GUI': Keycode.LEFT_GUI, 'RIGHT_CONTROL': Keycode.RIGHT_CONTROL, 'RIGHT_SHIFT': Keycode.RIGHT_SHIFT, 'RIGHT_ALT': Keycode.RIGHT_ALT, 'RIGHT_GUI': Keycode.RIGHT_GUI, 'LEFT_CONTROL': Keycode.LEFT_CONTROL, 'CTRL': Keycode.CONTROL, 'CONTROL': Keycode.CONTROL, "COMMAND": Keycode.COMMAND, "WINDOWS": Keycode.WINDOWS, "WIN": Keycode.WINDOWS, "OPTION": Keycode.ALT, "ALT": Keycode.ALT, "SHIFT": Keycode.SHIFT, "SPACE": Keycode.SPACE, "RETURN": Keycode.RETURN
 
 }
 
@@ -100,9 +81,9 @@ def parseLine(line):
     elif(line[0:12] == "DEFAULTDELAY"):
         defaultDelay = int(line[13:]) * 10
     elif(line[0:3] == "LED"):
-        if(led.value == True):
+        if (line[4:6] == "on"):
             led.value = False
-        else:
+        elif (line[4:7] == "off"):
             led.value = True
     else:
         newScriptLine = convertLine(line)
@@ -174,19 +155,22 @@ def selectPayload():
     keystatus5 = getKeyStatus(4)
 
     if(keystatus1 == True):
-        payload = "button1.dd"
+        payload = "button5.dd"
     elif(keystatus2 == True):
-        payload = "button2.dd"
+        payload = "button4.dd"
     elif(keystatus3 == True):
         payload = "button3.dd"
     elif(keystatus4 == True):
-        payload = "button4.dd"
+        payload = "button2.dd"
     elif(keystatus5 == True):
-        payload = "button5.dd"
+        payload = "button1.dd"
     else:
         return False
     return payload
 
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
 
 # runScript("button1.dd")
 while True:
