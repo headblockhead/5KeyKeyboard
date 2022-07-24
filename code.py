29ff241701abbaa480f0183301bcdad33c3c017b
Headblockhead <headblockhead@gmail.com>
2022-07-24 20:59:23 +0100

    Added mediaKeys

--- code.py
+++ code.py
@@ -5,8 +5,8 @@ from adafruit_hid.keyboard import Keyboard
 # https://kbdlayout.info/kbduk
 # https://github.com/Neradoc/Circuitpython_Keyboard_Layouts
 
-from adafruit_hid.keyboard_layout_win_uk import KeyboardLayout
-from adafruit_hid.keycode_win_uk import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayout
+from adafruit_hid.keycode import Keycode
 
 import supervisor
 
@@ -15,6 +15,8 @@ import digitalio
 from board import *
 import pwmio
 import board
+
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 defaultDelay = 0
 
@@ -25,6 +27,22 @@ layout = KeyboardLayout(kbd)
 supervisor.disable_autoreload()
 
 time.sleep(.5)
+
+duckyMediaCommands = {
+    "PLAY_PAUSE": ConsumerControlCode.PLAY_PAUSE,
+    "STOP": ConsumerControlCode.STOP,
+    "MUTE": ConsumerControlCode.MUTE,
+    "VOLUME_INCREMENT": ConsumerControlCode.VOLUME_UP,
+    "VOLUME_DECREMENT": ConsumerControlCode.VOLUME_DOWN,
+    "SCAN_NEXT_TRACK": ConsumerControlCode.NEXT,
+    "SCAN_PREVIOUS_TRACK": ConsumerControlCode.PREVIOUS,
+    "REWIND": ConsumerControlCode.REWIND,
+    "FAST_FORWARD": ConsumerControlCode.FAST_FORWARD,
+    "EJECT": ConsumerControlCode.EJECT,
+    "BRIGHTNESS_DECREMENT": ConsumerControlCode.BRIGHTNESS_DECREMENT,
+    "BRIGHTNESS_INCREMENT": ConsumerControlCode.BRIGHTNESS_INCREMENT,
+    "RECORD": ConsumerControlCode.RECORD,
+}
 
 duckyCommands = {
     'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E, 'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J, 'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O, 'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T, 'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y, 'Z': Keycode.Z, '1': Keycode.ONE, '2': Keycode.TWO, '3': Keycode.THREE, '4': Keycode.FOUR, '5': Keycode.FIVE, '6': Keycode.SIX, '7': Keycode.SEVEN, '8': Keycode.EIGHT, '9': Keycode.NINE, '0': Keycode.ZERO, 'ENTER': Keycode.ENTER, 'RETURN': Keycode.RETURN, 'ESCAPE': Keycode.ESCAPE, 'BACKSPACE': Keycode.BACKSPACE, 'TAB': Keycode.TAB, 'SPACEBAR': Keycode.SPACEBAR, 'SPACE': Keycode.SPACE, 'MINUS': Keycode.MINUS, 'EQUALS': Keycode.EQUALS, 'LEFT_BRACKET': Keycode.LEFT_BRACKET, 'RIGHT_BRACKET': Keycode.RIGHT_BRACKET, 'BACKSLASH': Keycode.BACKSLASH, 'POUND': Keycode.POUND, 'SEMICOLON': Keycode.SEMICOLON, 'QUOTE': Keycode.QUOTE, 'GRAVE_ACCENT': Keycode.GRAVE_ACCENT, 'COMMA': Keycode.COMMA, 'PERIOD': Keycode.PERIOD, 'FORWARD_SLASH': Keycode.FORWARD_SLASH, 'CAPS_LOCK': Keycode.CAPS_LOCK, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3, 'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7, 'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11, 'F12': Keycode.F12, 'PRINT_SCREEN': Keycode.PRINT_SCREEN, 'SCROLL_LOCK': Keycode.SCROLL_LOCK, 'PAUSE': Keycode.PAUSE, 'INSERT': Keycode.INSERT, 'HOME': Keycode.HOME, 'PAGE_UP': Keycode.PAGE_UP, 'DELETE': Keycode.DELETE, 'END': Keycode.END, 'PAGE_DOWN': Keycode.PAGE_DOWN, 'RIGHT_ARROW': Keycode.RIGHT_ARROW, 'LEFT_ARROW': Keycode.LEFT_ARROW, 'DOWN_ARROW': Keycode.DOWN_ARROW, 'UP_ARROW': Keycode.UP_ARROW, 'KEYPAD_NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'KEYPAD_FORWARD_SLASH': Keycode.KEYPAD_FORWARD_SLASH, 'KEYPAD_ASTERISK': Keycode.KEYPAD_ASTERISK, 'KEYPAD_MINUS': Keycode.KEYPAD_MINUS, 'KEYPAD_PLUS': Keycode.KEYPAD_PLUS, 'KEYPAD_ENTER': Keycode.KEYPAD_ENTER, 'KEYPAD_ONE': Keycode.KEYPAD_ONE, 'KEYPAD_TWO': Keycode.KEYPAD_TWO, 'KEYPAD_THREE': Keycode.KEYPAD_THREE, 'KEYPAD_FOUR': Keycode.KEYPAD_FOUR, 'KEYPAD_FIVE': Keycode.KEYPAD_FIVE, 'KEYPAD_SIX': Keycode.KEYPAD_SIX, 'KEYPAD_SEVEN': Keycode.KEYPAD_SEVEN, 'KEYPAD_EIGHT': Keycode.KEYPAD_EIGHT, 'KEYPAD_NINE': Keycode.KEYPAD_NINE, 'KEYPAD_ZERO': Keycode.KEYPAD_ZERO, 'KEYPAD_PERIOD': Keycode.KEYPAD_PERIOD, 'KEYPAD_BACKSLASH': Keycode.KEYPAD_BACKSLASH, 'APPLICATION': Keycode.APPLICATION, 'POWER': Keycode.POWER, 'KEYPAD_EQUALS': Keycode.KEYPAD_EQUALS, 'F13': Keycode.F13, 'F14': Keycode.F14, 'F15': Keycode.F15, 'F16': Keycode.F16, 'F17': Keycode.F17, 'F18': Keycode.F18, 'F19': Keycode.F19, 'F20': Keycode.F20, 'F21': Keycode.F21, 'F22': Keycode.F22, 'F23': Keycode.F23, 'F24': Keycode.F24, 'LEFT_SHIFT': Keycode.LEFT_SHIFT, 'LEFT_ALT': Keycode.LEFT_ALT, 'RIGHT_GUI': Keycode.LEFT_GUI, 'RIGHT_CONTROL': Keycode.RIGHT_CONTROL, 'RIGHT_SHIFT': Keycode.RIGHT_SHIFT, 'RIGHT_ALT': Keycode.RIGHT_ALT, 'RIGHT_GUI': Keycode.RIGHT_GUI, 'LEFT_CONTROL': Keycode.LEFT_CONTROL, 'CTRL': Keycode.CONTROL, 'CONTROL': Keycode.CONTROL, "COMMAND": Keycode.COMMAND, "WINDOWS": Keycode.WINDOWS, "WIN": Keycode.WINDOWS, "OPTION": Keycode.ALT, "ALT": Keycode.ALT, "SHIFT": Keycode.SHIFT, "SPACE": Keycode.SPACE, "RETURN": Keycode.RETURN
@@ -40,9 +58,13 @@ def convertLine(line):
         key = key.upper()
         # find the keycode for the command in the list
         command_keycode = duckyCommands.get(key, None)
+        command_mediacode = duckyMediaCommands.get(key, None)
         if command_keycode is not None:
             # if it exists in the list, use it
             newline.append(command_keycode)
+        elif command_mediacode is not None:
+            # if it's in the media commands list, use it
+            newline.append(command_mediacode)
         elif hasattr(Keycode, key):
             # if it's in the Keycode module, use it (allows any valid keycode)
             newline.append(getattr(Keycode, key))
@@ -55,7 +77,12 @@ def convertLine(line):
 
 def runScriptLine(line):
     for k in line:
-        kbd.press(k)
+        if k in duckyMediaCommands.values():
+            # if k is in the media commands list, use consumer_control.send to send it
+            consumer_control.send(k)
+        else:
+            # otherwise, use kbd.press to press it
+            kbd.press(k)
     kbd.release_all()
 
 
